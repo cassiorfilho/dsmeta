cc2fc158bdb9004cab25f4ad0d98a08ab8f197b4
Cassio Rodrigues <cassiomrfilho@gmail.com>
2022-11-15 23:27:59 -0300

    Other components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,22 @@
 import NotificationButton from "./components/NotificationButton/Index";
+import Header from "./components/Header/Index";
+import SalesCard from "./components/SalesCard/Index";
+
 
 function App() {
   return (
     <>
-      
-      <NotificationButton />
-      <NotificationButton />
-      <NotificationButton />
-    </>
-  );
+      <Header />
+      <main>
+        <section id="sales">
+          <div className="dsmeta-container">
+            <SalesCard />
+            <NotificationButton />
+          </div>
+        </section>
+        </main>
+      </>
+      );
 }
 
-export default App
+      export default App
